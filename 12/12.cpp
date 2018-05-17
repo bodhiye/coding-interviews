35302bcf9d69228cd74a3111d59f870ccc052ec0
叶琼州 <qiongzhouye@gmail.com>
2018-05-17 15:59:47 +0800

    更新

--- 12/12.cpp
+++ 12/12.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+double power(double base, int exponent)
+{
+	int exp = abs(exponent);
+	double ans = 1.0;
+	while (exp)
+	{
+		if (exp & 1)ans *= base;
+		base *= base;
+		exp >>= 1;
+	}
+	return exponent < 0 ? 1 / ans : ans;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	double a;
+	int b;
+	cin >> a >> b;
+	cout << power(a, b);
 	return 0;
 }

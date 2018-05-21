19804a8cf21f37388110f250f43f0f0079a1f229
叶琼州 <qiongzhouye@gmail.com>
2018-05-21 11:46:33 +0800

    更新

--- 14/14.cpp
+++ 14/14.cpp
@@ -2,9 +2,43 @@
 
 using namespace std;
 
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) :val(x), next(NULL) {}
+	ListNode() {}
+};
+
+ListNode* FindKthToTail(ListNode* pListHead, unsigned int k)
+{
+	ListNode *p, *q;
+	p = q = pListHead;
+	int i = 0;
+	for (; p != NULL; i++)
+	{
+		if (i >= k)
+			q = q->next;
+		p = p->next;
+	}
+	return i < k ? NULL : q;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	int n, k;
+	ListNode* L = new ListNode;
+	L->next = NULL;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		ListNode *p = new ListNode;
+		cin >> p->val;
+		p->next = L->next;
+		L->next = p;
+	}
+	cin >> k;
+	cout << FindKthToTail(L, k)->val;
 	return 0;
 }

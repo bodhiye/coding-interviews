acbbe5afe43f03596481dd38b37d1b11987c0a64
叶琼州 <qiongzhouye@gmail.com>
2018-05-27 12:27:17 +0800

    更新

--- 18/18.cpp
+++ 18/18.cpp
@@ -2,9 +2,57 @@
 
 using namespace std;
 
+struct TreeNode
+{
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	TreeNode() {}
+};
+
+TreeNode* newTree()
+{
+	TreeNode* node = new TreeNode;
+	int x;
+	cin >> x;
+	if (!x)node = NULL;
+	else
+	{
+		node->val = x;
+		node->left = newTree();
+		node->right = newTree();
+	}
+	return node;
+}
+
+void preOrder(TreeNode *pRoot)
+{
+	if (pRoot)
+	{
+		cout << pRoot->val << " ";
+		preOrder(pRoot->left);
+		preOrder(pRoot->right);
+	}
+}
+
+void Mirror(TreeNode *pRoot)
+{
+	if (pRoot == NULL)
+		return;
+	TreeNode *temp = pRoot->left;
+	pRoot->left = pRoot->right;
+	pRoot->right = temp;
+	Mirror(pRoot->left);
+	Mirror(pRoot->right);
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	TreeNode* pRoot = NULL;
+	pRoot = newTree();
+	Mirror(pRoot);
+	preOrder(pRoot);
 	return 0;
 }

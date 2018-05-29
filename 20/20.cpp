06b48691ede200d9ca363a1c36839f625f546749
叶琼州 <qiongzhouye@gmail.com>
2018-05-29 15:30:44 +0800

    更新

--- 20/20.cpp
+++ 20/20.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
+
+stack<int> s, smin;
+
+void push(int value)
+{
+	s.push(value);
+	if (smin.empty() || value <= smin.top())
+		smin.push(value);
+}
+
+void pop()
+{
+	if (s.top() == smin.top())
+		smin.pop();
+	s.pop();
+}
+
+int top()
+{
+	return s.top();
+}
+
+int min()
+{
+	return smin.top();
+}
 
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	int temp;
+	while (1)
+	{
+		cin >> temp;
+		if (!temp)
+			break;
+		push(temp);
+	}
+	cout << min();
 	return 0;
 }

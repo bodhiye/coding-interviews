421e9ebe7c314ddcfdc28cb8e70c59f646896786
叶琼州 <qiongzhouye@gmail.com>
2018-06-05 20:59:24 +0800

    更新

--- 23/23.cpp
+++ 23/23.cpp
@@ -3,18 +3,36 @@
 
 using namespace std;
 
+//·ÇµÝ¹é½â·¨
+//bool VerifySquenceOfBST(vector<int> sequence)
+//{
+//	int len = sequence.size();
+//	if (!len)return false;
+//	while (--len)
+//	{
+//		int i = 0;
+//		while (sequence[i++] < sequence[len]);
+//		while (sequence[i++] > sequence[len]);
+//		if (i < len)return false;
+//	}
+//	return true;
+//}
+
+bool judge(vector<int>& v, int l, int r)
+{
+	if (l >= r)return true;
+	int i = r;
+	while (i > l&&v[--i] > v[r]);
+	for (int j = i - 1; j >= l; j--)
+		if (v[j] > v[r])return false;
+	return judge(v, l, i - 1) && judge(v, i, r - 1);
+}
+
 bool VerifySquenceOfBST(vector<int> sequence)
 {
 	int len = sequence.size();
 	if (!len)return false;
-	while (--len)
-	{
-		int i = 0;
-		while (sequence[i++] < sequence[len]);
-		while (sequence[i++] > sequence[len]);
-		if (i < len)return false;
-	}
-	return true;
+	return judge(sequence, 0, len - 1);
 }
 
 int main()

85919009f4b20c37bd8416eb7c2e37cdaf0176ec
叶琼州 <qiongzhouye@gmail.com>
2018-06-08 14:05:34 +0800

    更新

--- 31/31.cpp
+++ 31/31.cpp
@@ -2,9 +2,29 @@
 
 using namespace std;
 
+int NumberOf1Between1AndN_Solution(int n)
+{
+	if (n < 1)return 0;
+	int count = 0, base = 1, round = n;
+	while (round)
+	{
+		int i = round % 10;
+		round /= 10;
+		count += round * base;
+		if (i == 1)
+			count += (n % base + 1);
+		else if (i > 1)
+			count += base;
+		base *= 10;
+	}
+	return count;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	int n;
+	cin >> n;
+	cout << NumberOf1Between1AndN_Solution(n);
 	return 0;
 }

0fde0cc08bae46207aba8ae107c03242a1295294
叶琼州 <qiongzhouye@gmail.com>
2018-06-12 10:44:18 +0800

    更新

--- 32/32.cpp
+++ 32/32.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+static bool cmp(int &a, int &b)
+{
+	string aa = to_string(a);
+	string bb = to_string(b);
+	return (aa + bb) < (bb + aa);
+}
+
+string PrintMinNumber(vector<int> numbers)
+{
+	string res;
+	sort(numbers.begin(), numbers.end(), cmp);
+	vector<int>::iterator it;
+	for (it = numbers.begin(); it != numbers.end(); it++)
+	{
+		int temp = *it;
+		res += to_string(temp);
+	}
+	return res;
+}
 
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	vector<int> v;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	cout << PrintMinNumber(v);
 	return 0;
 }

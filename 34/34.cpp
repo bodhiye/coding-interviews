754240db18c2b343978a83930e497c1cf5a182cc
叶琼州 <qiongzhouye@gmail.com>
2018-06-13 10:34:20 +0800

    更新

--- 34/34.cpp
+++ 34/34.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
+
+int FirstNotRepeatingChar(string str)
+{
+	map<char, int> m;
+	for (int i = 0; i < str.length(); i++)
+		m[str[i]]++;
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (m[str[i]] == 1)
+			return i;
+	}
+	return -1;
+}
 
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	string s;
+	cin >> s;
+	cout << FirstNotRepeatingChar(s);
 	return 0;
 }

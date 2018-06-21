e7476903fd1f01ce68ad86388e28ee03aca30cb5
叶琼州 <qiongzhouye@gmail.com>
2018-06-21 08:49:11 +0800

    数组中的逆序对

--- 35/35.cpp
+++ 35/35.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 
 using namespace std;
-int temp[100001];
-int cnt;
 
-void merge(vector<int> a, int start, int mid, int end)
+int temp[200001];
+long long cnt = 0;
+
+void merge(vector<int> &a, int start, int mid, int end)
 {
 	int i = start, j = mid + 1, index = 0;
 	while (i <= mid && j <= end)
@@ -14,7 +15,7 @@ void merge(vector<int> a, int start, int mid, int end)
 			temp[index++] = a[i++];
 		else
 		{
-			cnt += (mid - i + 1) % 1000000007;
+			cnt += (mid - i + 1);
 			temp[index++] = a[j++];
 		}
 	}
@@ -24,36 +25,36 @@ void merge(vector<int> a, int start, int mid, int end)
 		a[start + i] = temp[i];
 }
 
-void mergesort(vector<int> a, int start, int end)
+void mergesort(vector<int> &data, int start, int end)
 {
 	if (start < end)
 	{
 		int mid = (start + end) >> 1;
-		mergesort(a, start, mid);
-		mergesort(a, mid + 1, end);
-		merge(a, start, mid, end);
+		mergesort(data, start, mid);
+		mergesort(data, mid + 1, end);
+		merge(data, start, mid, end);
 	}
 }
 
 int InversePairs(vector<int> data)
 {
 	int len = data.size();
-	if (!len) return 0;
+	if (!len)return 0;
 	mergesort(data, 0, len - 1);
-	return cnt;
+	return cnt % 1000000007;
 }
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	vector<int> v;
+	vector<int> a;
 	int n, tmp;
 	cin >> n;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> tmp;
-		v.push_back(tmp);
+		a.push_back(tmp);
 	}
-	cout << InversePairs(v);
+	cout << InversePairs(a);
 	return 0;
 }

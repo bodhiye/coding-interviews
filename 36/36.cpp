a91ffcab546ee6798a1b5a82f128744c2255f67b
叶琼州 <qiongzhouye@gmail.com>
2018-06-21 08:50:11 +0800

    两个链表的第一个公共结点

--- 36/36.cpp
+++ 36/36.cpp
@@ -2,9 +2,49 @@
 
 using namespace std;
 
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode(int x) :val(x), next(NULL) {}
+	ListNode() {}
+};
+
+ListNode* newList()
+{
+	ListNode* L = new ListNode;
+	L->next = NULL;
+	ListNode* q = L;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		ListNode *p = new ListNode;
+		cin >> p->val;
+		q->next = p;
+		q = p;
+	}
+	q->next = NULL;
+	return q;
+}
+
+ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2)
+{
+	ListNode* p1 = pHead1;
+	ListNode* p2 = pHead2;
+	while (p1 != p2)
+	{
+		p1 = (p1 != NULL ? p1->next : pHead2);
+		p2 = (p2 != NULL ? p2->next : pHead1);
+	}
+	return p1;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	ListNode* L1 = newList();
+	ListNode* L2 = newList();
+	cout << FindFirstCommonNode(L1, L2)->val;
 	return 0;
 }

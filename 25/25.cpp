52e90de667bc6a6cc00972a618d03c25640d65b3
叶琼州 <qiongzhouye@gmail.com>
2018-06-26 11:50:27 +0800

    复杂链表的复制

--- 25/25.cpp
+++ 25/25.cpp
@@ -2,9 +2,77 @@
 
 using namespace std;
 
+struct RandomListNode
+{
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+	RandomListNode() {}
+};
+
+RandomListNode* newList()
+{
+	RandomListNode* L = new RandomListNode;
+	L->next = NULL;
+	L->random = NULL;
+	RandomListNode* q = L;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		RandomListNode *p = new RandomListNode;
+		cin >> p->label;
+		q->next = p;
+		q->random = p;
+		q = p;
+	}
+	q->next = NULL;
+	q->random = NULL;
+	return L;
+}
+
+RandomListNode* Clone(RandomListNode* pHead)
+{
+	if (!pHead) return NULL;
+	RandomListNode* currNode = pHead;
+	while (currNode)
+	{
+		RandomListNode* node = new RandomListNode(currNode->label);
+		node->next = currNode->next;
+		currNode->next = node;
+		currNode = node->next;
+	}
+	currNode = pHead;
+	while (currNode)
+	{
+		RandomListNode* node = currNode->next;
+		if (currNode->random)
+			node->random = currNode->random->next;
+		currNode = node->next;
+	}
+	RandomListNode* pCloneHead = pHead->next;
+	RandomListNode* tmp;
+	currNode = pHead;
+	while (currNode->next)
+	{
+		tmp = currNode->next;
+		currNode->next = tmp->next;
+		currNode = tmp;
+	}
+	return pCloneHead;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	RandomListNode* L1 = newList();
+	RandomListNode* L2 = Clone(L1);
+	RandomListNode* p = L2->next;
+	while (p)
+	{
+		cout << p->label << " ";
+		p = p->next;
+	}
+	cout << endl;
 	return 0;
 }

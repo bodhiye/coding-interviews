cc8bf61a31bb4c132a1d747ae4a28887897d1696
叶琼州 <qiongzhouye@gmail.com>
2018-06-29 16:27:31 +0800

    字符串的排列

--- 27/27.cpp
+++ 27/27.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+vector<string> res;
+
+void perm(string str, int begin)
+{
+	if (begin == str.length())
+		res.push_back(str);
+	else
+	{
+		for (int i = begin; i < str.length(); i++)
+		{
+			if (i != begin && str[i] == str[begin]) continue;
+			swap(str[begin], str[i]);
+			perm(str, begin + 1);
+			swap(str[begin], str[i]);
+		}
+	}
+}
+
+vector<string> Permutation(string str)
+{
+	if (str.length() == 0) return res;
+	perm(str, 0);
+	sort(res.begin(), res.end());
+	return res;
+}
 
 int main()
 {
 	ios::sync_with_stdio(false);
-
+	string s;
+	cin >> s;
+	vector<string> v = Permutation(s);
+	vector<string>::iterator it;
+	for (it = v.begin(); it != v.end(); it++)
+		cout << *it << endl;
 	return 0;
 }

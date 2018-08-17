a7a1ff86cf7a4411db58eabc03f854433434ae44
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-17 16:20:45 +0800

    机器人的运动范围

--- 66/66.cpp
+++ 66/66.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int getSum(int n) {
+    int sum = 0;
+    while (n) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
 
-	return 0;
+int moving(int threshold, int i, int j, int rows, int cols, vector<vector<int> > &flag) {
+    if (i < 0 || i >= rows || j < 0 || j >= cols || flag[i][j] == 1 || getSum(i) + getSum(j) > threshold)
+        return 0;
+    flag[i][j] = 1;
+    return moving(threshold, i - 1, j, rows, cols, flag) + moving(threshold, i + 1, j, rows, cols, flag) +
+           moving(threshold, i, j - 1, rows, cols, flag) + moving(threshold, i, j + 1, rows, cols, flag) + 1;
+}
+
+int movingCount(int threshold, int rows, int cols) {
+    vector<vector<int> > flag(rows);
+    for (int i = 0; i < rows; i++)
+        flag[i].resize(cols, 0);
+    return moving(threshold, 0, 0, rows, cols, flag);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int m, n, k;
+    cin >> m >> n >> k;
+    cout << movingCount(k, m, n);
+    return 0;
 }

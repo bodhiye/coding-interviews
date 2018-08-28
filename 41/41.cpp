8789ec0c1ae86bab9542ef280b75548a68cf1ead
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-28 10:12:46 +0800

    和为S的连续正数序列

--- 41/41.cpp
+++ 41/41.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+vector<vector<int> > FindContinuousSequence(int sum) {
+    vector<vector<int> > res;
+    int low = 1, high = 2;
+    while (low < high) {
+        int tempsum = ((low + high) * (high - low + 1) >> 1);
+        if (tempsum == sum) {
+            vector<int> temp;
+            for (int i = low; i <= high; i++)
+                temp.push_back(i);
+            res.push_back(temp);
+            low++;
+        } else if (tempsum < sum)
+            high++;
+        else
+            low++;
+    }
+    return res;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    int sum;
+    vector<vector<int> > res;
+    vector<int> temp;
+    cin >> sum;
+    res = FindContinuousSequence(sum);
+    vector<vector<int> >::iterator iter;
+    vector<int>::iterator it;
+    for (iter = res.begin(); iter != res.end(); iter++) {
+        temp = *iter;
+        for (it = temp.begin(); it != temp.end(); it++)
+            cout << *it << " ";
+        cout << endl;
+    }
+    return 0;
 }

2cfcf42d44fe1731ff448bb3fe23e1eb00133c4e
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-28 22:38:50 +0800

    不用加减乘除做加法

--- 48/48.cpp
+++ 48/48.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int Add(int num1, int num2) {
+    while (num2) {
+        int carry = num1 ^num2;
+        num2 = (num1 & num2) << 1;
+        num1 = carry;
+    }
+    return num1;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    int a, b;
+    cin >> a >> b;
+    cout << Add(a, b);
+    return 0;
 }

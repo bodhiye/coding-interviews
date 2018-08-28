44bdac4e16f43e0b6f8397d248011246cb7d896f
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-28 15:37:37 +0800

    孩子们的游戏(圆圈中最后剩下的数)

--- 46/46.cpp
+++ 46/46.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int LastRemaining_Solution(int n, int m) {
+    if (n < 1 || m < 1)return -1;
+    int count = n, step = 0, i = -1;
+    vector<int> v(n);
+    while (count) {
+        i++;
+        i = i % n;
+        if (v[i] == -1)continue;
+        step++;
+        if (step == m) {
+            v[i] = -1;
+            step = 0;
+            count--;
+        }
+    }
+    return i;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    int n, m;
+    cin >> n >> m;
+    cout << LastRemaining_Solution(n, m);
+    return 0;
 }

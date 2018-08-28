c8db86df8cbb108c0959e957d0bf1d6ea467e109
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-28 11:38:57 +0800

    翻转单词顺序列

--- 44/44.cpp
+++ 44/44.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+string ReverseSentence(string str) {
+    string res = "", temp = "";
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ' ') {
+            res = " " + temp + res;
+            temp = "";
+        } else temp += str[i];
+    }
+    res = temp + res;
+    return res;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    string s;
+    getline(cin, s);
+    cout << ReverseSentence(s);
+    return 0;
 }

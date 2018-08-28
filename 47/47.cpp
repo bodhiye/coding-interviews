6d55d190b593e7381348016feec66c1407c8a718
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-28 18:08:28 +0800

    求1+2+3+...+n

--- 47/47.cpp
+++ 47/47.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int Sum_Solution(int n) {
+    int res = n;
+    res && (res += Sum_Solution(n - 1));
+    return res;
+}
 
-	return 0;
+int Sum_Solution1(int n) {
+    bool a[n][n + 1];
+    return sizeof(a) >> 1;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    cout << Sum_Solution(n) << endl;
+    cout << Sum_Solution1(n);
+    return 0;
 }

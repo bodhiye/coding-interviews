5a5beadc179651672804782e6477fa7352b1e3c2
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-28 11:23:48 +0800

    左旋转字符串

--- 43/43.cpp
+++ 43/43.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+string LeftRotateString(string str, int n) {
+    int len = str.length();
+    str += str;
+    if (len == 0)
+        return "";
+    n = n % len;
+    return str.substr(n, len);
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    string s;
+    cin >> s;
+    int n;
+    cin >> n;
+    cout << LeftRotateString(s, n);
+    return 0;
 }

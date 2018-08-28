e1239b6eee73943cf548005b7547ef8230354b06
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-28 14:44:10 +0800

    扑克牌顺子

--- 45/45.cpp
+++ 45/45.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+bool IsContinuous(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end());
+    int len = numbers.size(), zero = 0, internal = 0;
+    if (len == 0)return false;
+    for (int i = 0; i < len - 1; i++) {
+        if (numbers[i] == 0) {
+            zero++;
+            continue;
+        }
+        if (numbers[i] == numbers[i + 1])return false;
+        internal += (numbers[i + 1] - numbers[i] - 1);
+    }
+    if (zero >= internal)return true;
+    return false;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    vector<int> v;
+    int n, temp;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        v.push_back(temp);
+    }
+    cout << IsContinuous(v);
+    return 0;
 }

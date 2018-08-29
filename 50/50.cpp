2c2ccdc878dc7e41c0fc5c93ed01d00ee8b0b87a
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-29 12:30:52 +0800

    数组中重复的数字

--- 50/50.cpp
+++ 50/50.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+bool duplicate(int numbers[], int length, int *duplication) {
+    vector<bool> b = {0};
+    for (int i = 0; i < length; i++) {
+        if (b[numbers[i]]) {
+            *duplication = numbers[i];
+            return true;
+        }
+        b[numbers[i]] = true;
+    }
+    return false;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    int n, res;
+    int a[1001];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << duplicate(a, n, &res) << endl;
+    cout << res;
+    return 0;
 }

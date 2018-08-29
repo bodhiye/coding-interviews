1eb8481372e528930a42b1cc37e6294cb4cc9ded
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-29 10:15:24 +0800

    把字符串转换成整数

--- 49/49.cpp
+++ 49/49.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int StrToInt(string str) {
+    int len = str.length();
+    int flag = 1;
+    long long res = 0;
+    if (!len)return 0;
+    if (str[0] == '-')flag = -1;
+    for (int i = (str[0] == '+' || str[0] == '-') ? 1 : 0; i < len; i++) {
+        if (!(str[i] >= '0' && str[i] <= '9'))return 0;
+        res = res * 10 + (str[i] - '0');
+    }
+    return res * flag;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    string s;
+    cin >> s;
+    cout << StrToInt(s);
+    return 0;
 }

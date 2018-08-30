275aa27619ccaf8b45139b04a2fb37c72393bfdf
叶琼州 <bodhiye@users.noreply.github.com>
2018-08-30 10:23:27 +0800

    构建乘积数组

--- 51/51.cpp
+++ 51/51.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+vector<int> multiply(const vector<int> &A) {
+    int len = A.size();
+    vector<int> B(len);
+    B[0] = 1;
+    for (int i = 1; i < len; i++)
+        B[i] = B[i - 1] * A[i - 1];
+    int temp = 1;
+    for (int i = len - 2; i >= 0; i--) {
+        temp *= A[i + 1];
+        B[i] *= temp;
+    }
+    return B;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    vector<int> a, b;
+    int temp, n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        a.push_back(temp);
+    }
+    b = multiply(a);
+    for (int i = 0; i < n; i++)
+        cout << b[i] << " ";
+    return 0;
 }

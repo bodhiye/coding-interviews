d3ebc2a6f381165d0e7d9a1cdf284adab50edbce
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-05 14:40:27 +0800

    字符流中第一个不重复的字符

--- 54/54.cpp
+++ 54/54.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+map<char, int> m;
+vector<int> v;
 
-	return 0;
+void Insert(char ch) {
+    v.push_back(ch);
+    m[ch]++;
+}
+
+char FirstAppearingOnce() {
+    for (auto it = v.begin(); it != v.end(); ++it)
+        if (m[*it] == 1)
+            return *it;
+    return '#';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    char c;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> c;
+        Insert(c);
+    }
+    cout << FirstAppearingOnce();
+    return 0;
 }

0256cfd570d0e31d7a915b28a304c317300aaa87
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-05 10:16:00 +0800

    正则表达式匹配

--- 52/52.cpp
+++ 52/52.cpp
@@ -2,9 +2,28 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+bool match(char *str, char *pattern) {
+    if (*str == '\0' && *pattern == '\0')
+        return true;
+    if (*str != '\0' && *pattern == '\0')
+        return false;
+    if (*(pattern + 1) != '*') {
+        if (*str == *pattern || (*str != '\0' && *pattern == '.'))
+            return match(str + 1, pattern + 1);
+        else
+            return false;
+    } else {
+        if (*str == *pattern || (*str != '\0' && *pattern == '.'))
+            return match(str, pattern + 2) || match(str + 1, pattern);
+        else
+            return match(str, pattern + 2);
+    }
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    char a[101], b[101];
+    cin >> a >> b;
+    cout << match(a, b);
+    return 0;
 }

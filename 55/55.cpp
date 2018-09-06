92e58debeb7638a453c541376da85fdd55fffad2
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-06 18:12:28 +0800

    链表中环的入口结点

--- 55/55.cpp
+++ 55/55.cpp
@@ -2,9 +2,54 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+struct ListNode {
+    int val;
+    ListNode *next;
 
-	return 0;
+    ListNode(int x) : val(x), next(NULL) {}
+
+    ListNode() {}
+};
+
+ListNode *newList() {
+    ListNode *L = new ListNode;
+    L->next = NULL;
+    ListNode *q = L;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        ListNode *p = new ListNode;
+        cin >> p->val;
+        q->next = p;
+        q = p;
+    }
+    q->next = NULL;
+    return L->next;
+}
+
+ListNode *EntryNodeOfLoop(ListNode *pHead) {
+    if (pHead == NULL || pHead->next == NULL)
+        return NULL;
+    ListNode *p1 = pHead, *p2 = pHead;
+    while (p2 != NULL && p2->next != NULL) {
+        p1 = p1->next;
+        p2 = p2->next->next;
+        if (p1 == p2) {
+            p2 = pHead;
+            while (p1 != p2) {
+                p1 = p1->next;
+                p2 = p2->next;
+            }
+            if (p1 == p2)
+                return p1;
+        }
+    }
+    return NULL;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    ListNode *L = newList();
+    cout << EntryNodeOfLoop(L)->val;
+    return 0;
 }

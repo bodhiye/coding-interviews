afaabfbb5081aa683b6da54c9524e33e07fe521c
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-09 16:41:09 +0800

    滑动窗口的最大值

--- 64/64.cpp
+++ 64/64.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <deque>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+vector<int> maxInWindows(const vector<int> &num, unsigned int size) {
+    vector<int> v;
+    deque<int> d;
+    for (unsigned int i = 0; i < num.size(); ++i) {
+        while (d.size() && num[d.back()] <= num[i])
+            d.pop_back();
+        if (d.size() && i - d.front() + 1 > size)
+            d.pop_front();
+        d.push_back(i);
+        if (size && i + 1 >= size)
+            v.push_back(num[d.front()]);
+    }
+    return v;
+}
 
-	return 0;
+int main() {
+    ios::sync_with_stdio(false);
+    vector<int> v, res;
+    int n, k, tmp;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    res = maxInWindows(v, k);
+    for (auto it = res.begin(); it != res.end(); ++it)
+        cout << *it << " ";
+    return 0;
 }

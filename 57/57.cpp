c79531563ee1029bd9353c06c7cfa33c7e19ffe4
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-09 18:18:51 +0800

    二叉树的下一个结点

--- 57/57.cpp
+++ 57/57.cpp
@@ -2,9 +2,52 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
 
-	return 0;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+
+    TreeLinkNode() {}
+};
+
+TreeLinkNode *newTree() {
+    TreeLinkNode *node = new TreeLinkNode;
+    int x;
+    cin >> x;
+    if (!x)node = NULL;
+    else {
+        node->val = x;
+        node->left->next = node;
+        node->left = newTree();
+        node->right->next = node;
+        node->right = newTree();
+    }
+    return node;
+}
+
+TreeLinkNode *GetNext(TreeLinkNode *pNode) {
+    if (!pNode)return NULL;
+    if (pNode->right) {
+        pNode = pNode->right;
+        while (pNode->left)pNode = pNode->left;
+        return pNode;
+    }
+    while (pNode->next) {
+        if (pNode->next->left == pNode)
+            return pNode->next;
+        pNode = pNode->next;
+    }
+    return NULL;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    TreeLinkNode *node = newTree();
+    TreeLinkNode *pNode;
+    TreeLinkNode *res = GetNext(pNode);
+    cout << res->val;
+    return 0;
 }

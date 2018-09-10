44d5bfea7ef41bed5a53556396c6901b75e977f3
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-10 17:01:30 +0800

    数据流中的中位数

--- 63/63.cpp
+++ 63/63.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+vector<int> v;
+int n;
 
-	return 0;
+void Insert(int num) {
+    v.push_back(num);
+    n = v.size();
+    for (int i = n - 1; i > 0 && v[i] < v[i - 1]; --i)
+        swap(v[i], v[i - 1]);
+}
+
+double GetMedian() {
+    return (v[(n - 1) >> 1] + v[n >> 1]) / 2.0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int m, tmp;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> tmp;
+        Insert(tmp);
+    }
+    cout << GetMedian();
+    return 0;
 }

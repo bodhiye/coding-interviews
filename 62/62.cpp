22f8473b98e2efc7e2dd457d8bbfc7d4cb27bb46
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-10 16:44:07 +0800

    二叉搜索树的第k个结点

--- 62/62.cpp
+++ 62/62.cpp
@@ -1,10 +1,52 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
 
-	return 0;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+
+    TreeNode() {}
+};
+
+TreeNode *newTree() {
+    TreeNode *node = new TreeNode;
+    int x;
+    cin >> x;
+    if (!x)node = NULL;
+    else {
+        node->val = x;
+        node->left = newTree();
+        node->right = newTree();
+    }
+    return node;
+}
+
+int count = 0;
+
+TreeNode *KthNode(TreeNode *pRoot, int k) {
+    if (pRoot) {
+        TreeNode *node = KthNode(pRoot->left, k);
+        if (node) return node;
+        if (++count == k)return pRoot;
+        node = KthNode(pRoot->right, k);
+        if (node)return node;
+    }
+    return NULL;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int k;
+    cin >> k;
+    TreeNode *root = NULL;
+    root = newTree();
+    cout << KthNode(root, k)->val;
+    return 0;
 }

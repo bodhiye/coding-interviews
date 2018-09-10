4621d1a1471b205296a4eed6c9da4aa1867964ee
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-10 12:36:45 +0800

    按之字形顺序打印二叉树

--- 59/59.cpp
+++ 59/59.cpp
@@ -1,10 +1,69 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
 
-	return 0;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+
+    TreeNode() {}
+};
+
+TreeNode *newTree() {
+    TreeNode *node = new TreeNode;
+    int x;
+    cin >> x;
+    if (!x)node = NULL;
+    else {
+        node->val = x;
+        node->left = newTree();
+        node->right = newTree();
+    }
+    return node;
+}
+
+vector<vector<int> > Print(TreeNode *pRoot) {
+    vector<vector<int> > res;
+    if (!pRoot)return res;
+    queue<TreeNode *> q;
+    q.push(pRoot);
+    bool even = false;
+    while (!q.empty()) {
+        vector<int> v;
+        int size=q.size();
+        for (int i = 0; i < size; ++i) {
+            TreeNode *tmp = q.front();
+            q.pop();
+            v.push_back(tmp->val);
+            if (tmp->left)q.push(tmp->left);
+            if (tmp->right)q.push(tmp->right);
+        }
+        if (even)
+            reverse(v.begin(), v.end());
+        even = !even;
+        res.push_back(v);
+    }
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    TreeNode *root = NULL;
+    root = newTree();
+    vector<vector<int> > res;
+    res = Print(root);
+    for (auto it = res.begin(); it != res.end(); ++it) {
+        for (auto i = (*it).begin(); i != (*it).end(); ++i)
+            cout << *i << " ";
+        cout << endl;
+    }
+    return 0;
 }

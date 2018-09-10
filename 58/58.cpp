9c172cb31bb32b151e273c35de42c5a99273d2de
叶琼州 <bodhiye@users.noreply.github.com>
2018-09-10 10:19:57 +0800

    对称的二叉树

--- 58/58.cpp
+++ 58/58.cpp
@@ -2,9 +2,50 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(false);
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
 
-	return 0;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+
+    TreeNode() {}
+};
+
+TreeNode *newTree() {
+    TreeNode *node = new TreeNode;
+    int x;
+    cin >> x;
+    if (!x)node = NULL;
+    else {
+        node->val = x;
+        node->left = newTree();
+        node->right = newTree();
+    }
+    return node;
+}
+
+bool symmetrical(TreeNode *left, TreeNode *right) {
+    if (!left && !right)
+        return true;
+    if (left && right)
+        return left->val == right->val && symmetrical(left->left, right->right) &&
+               symmetrical(left->right, right->left);
+    return false;
+}
+
+bool isSymmetrical(TreeNode *pRoot) {
+    if (!pRoot)
+        return true;
+    return symmetrical(pRoot->left, pRoot->right);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    TreeNode *root = NULL;
+    root = newTree();
+    cout << isSymmetrical(root);
+    return 0;
 }
